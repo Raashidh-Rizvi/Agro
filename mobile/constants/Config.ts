c4633e633f802733edf8b4dd65a416db3a8b8424
Dinithi Yasasvi <149082480+Dinyyyd@users.noreply.github.com>
2026-05-03 12:50:15 +0530

    Update backend IP address in Config.ts

--- mobile/constants/Config.ts
+++ mobile/constants/Config.ts
@@ -5,7 +5,7 @@ const getBaseUrl = () => {
         return process.env.EXPO_PUBLIC_API_URL.replace('/api', '');
     }
     if (Platform.OS === 'web') return 'http://localhost:5000';
-    return 'http://172.20.10.2:5000';
+    return 'http://192.168.1.6:5000';
 };
 
 export const BASE_URL = getBaseUrl();
